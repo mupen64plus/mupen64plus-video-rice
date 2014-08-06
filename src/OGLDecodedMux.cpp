8df95b38de70f8610df34aeb1781283d0421a586
Dorian Fevrier <fevrier.dorian@yahoo.fr>
2014-08-05 22:08:51 -0400

    remove useless inheritance in OGLCombiner

--- src/OGLDecodedMux.cpp
+++ src/OGLDecodedMux.cpp
@@ -18,12 +18,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "OGLDecodedMux.h"
 
-//========================================================================
-void COGLDecodedMux::Simplify(void)
-{
-    DecodedMux::Simplify();
-}
-
 void COGLDecodedMux::Reformat(void)
 {
     DecodedMux::Reformat();
