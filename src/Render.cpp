6b9b76c57e5cbb98a57568382bda8ade282d1188
Dorian Fevrier <fevrier.dorian@yahoo.fr>
2015-01-06 23:51:18 -0500

    fix some filtering problems. Good filter where applied on texture unit 0 and texture unit 1 but other textures where binded to those units _after_ without set the filter. This commit place the applyFilter function at a more efficient place

--- src/Render.cpp
+++ src/Render.cpp
@@ -326,6 +326,8 @@ void CRender::SetCombinerAndBlender()
         m_pAlphaBlender->InitBlenderMode();
 
     m_pColorCombiner->InitCombinerMode();
+
+    ApplyTextureFilter();
 }
 
 void CRender::RenderReset()
@@ -1843,8 +1845,6 @@ void CRender::UpdateScissorWithClipRatio()
 // Set other modes not covered by color combiner or alpha blender
 void CRender::InitOtherModes(void)
 {
-    ApplyTextureFilter();
-
     //
     // I can't think why the hand in mario's menu screen is rendered with an opaque rendermode,
     // and no alpha threshold. We set the alpha reference to 1 to ensure that the transparent pixels
