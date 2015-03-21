b40f3babcbf59398fd981d04d69e5df7bce1d80e
Conchúr Navid <conchur@web.de>
2015-03-21 13:19:02 +0100

    Fix build error because of missing GL type definitions

--- src/OGLRender.h
+++ src/OGLRender.h
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "Combiner.h"
 #include "Render.h"
+#include "osal_opengl.h"
 
 class OGLRender : public CRender
 {
